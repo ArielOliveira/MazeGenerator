db8570bac62d937aa9b5d8dc0a0f118303c060bf
ArielOliveira <ariel.oliveira01@gmail.com>
2020-02-03 07:54:28 -0300

    Working Cells

--- src/backtracker.c
+++ src/backtracker.c
@@ -1,18 +1,21 @@
 #include "../include/backtracker.h"
 #include "../tools/stack.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int randomizeInt(int max) {
-	int randomN;
-	randomN = ((int)rand()/(int)(RAND_MAX) * max);
+	int randomN = 0;
+	while(!randomN) randomN = rand() % max;
 	return randomN;
 }
 
 void carve(int **map, int x, int y) {
 	int i, j;
-	for (i = x; i < x+CELL_SIZE-1; i++) {
-	       for (j = y; j < y+CELL_SIZE-1; j++) {
+	int nextCellX = x*CELL_SIZE;
+	int nextCellY = y*CELL_SIZE;
+	for (i = nextCellX-CELL_SIZE; i < nextCellX-1; i++) {
+	       for (j = nextCellY-CELL_SIZE; j < nextCellY-1; j++) {
 		       map[i][j] = 1;
 	       }	       
 	}
@@ -20,10 +23,10 @@ void carve(int **map, int x, int y) {
 }
 
 void create_map(int **map) {
+	srand(time(NULL));
 	if (!map) {
 		exit(1);
 	} else {
-		srand(time(NULL));
 		int i,j;
 		for (i = 0; i < MAP_SIZE; i++) {
 			for (j = 0; j < MAP_SIZE; j++) {
@@ -31,8 +34,10 @@ void create_map(int **map) {
 			}
 		}
 
-		int first_x = randomizeInt(MAP_SIZE-1);
-		int first_y = randomizeInt(MAP_SIZE-1);
+		int first_x = randomizeInt(MAP_SIZE/CELL_SIZE);
+		int first_y = randomizeInt(MAP_SIZE/CELL_SIZE);
+
+		printf("/n %i %i/n", first_x, first_y); 
 
 		push(first_x, first_y);
 		carve(map, first_x, first_y);
