c269f0ffc73052731ec3fca5524d21716385f142
ArielOliveira <ariel.oliveira01@gmail.com>
2020-02-04 08:00:01 -0300

    all gone wrong

--- include/backtracker.h
+++ include/backtracker.h
@@ -7,6 +7,7 @@
 typedef enum {
        	UNVISITED,
 	VISITED,
+	WALL
 } Status;
 
 typedef struct Node {
--- src/backtracker.c
+++ src/backtracker.c
@@ -14,6 +14,7 @@ void carve_cell(int **map, Cell cell) {
 	int i, j;
 	cell.x *= CELL_SIZE;
 	cell.y *= CELL_SIZE;
+	printf("%i %i", cell.x, cell.y);
 	for (i = cell.x-CELL_SIZE; i < cell.x-1; i++) {
 	       for (j = cell.y-CELL_SIZE; j < cell.y-1; j++) {
 		       map[i][j] = VISITED;
@@ -34,25 +35,26 @@ Cell* sort_cell(int **map, int x, int y) {
 	int i = 0;
 	
 	Cell nav[4];
-	nav[0].x = ((x+1)*CELL_SIZE);
+	nav[0].x = ((x+2)*CELL_SIZE);
 	nav[0].y = (y*CELL_SIZE);
 
-	nav[1].x = ((x-1)*CELL_SIZE);
+	nav[1].x = ((x-2)*CELL_SIZE);
 	nav[1].y = (y*CELL_SIZE);
 
 	nav[2].x = (x*CELL_SIZE);
-	nav[2].y = ((y+1)*CELL_SIZE);
+	nav[2].y = ((y+2)*CELL_SIZE);
 
 	nav[3].x = (x*CELL_SIZE);
-	nav[3].y = ((y-1)*CELL_SIZE);
-	
+	nav[3].y = ((y-2)*CELL_SIZE);
+	printf(" %i %i ", x, y);
 
 	Cell *valid_nav = (Cell*)malloc(sizeof(Cell));
 	valid_nav->array = 0;
 	for (i = 0; i < 4; i++) {
-		if ((nav[i].x > 1 && nav[i].y > 1) &&
-			       	(nav[i].x < MAP_SIZE && nav[i].y < MAP_SIZE)) {
-			if (map[nav[i].x-1][nav[i].y-1] == UNVISITED) {
+		printf(" ======########%i %i======####### ", nav[i].x, nav[i].y);
+		if ((nav[i].x > 16 && nav[i].y > 16) &&
+			       	(nav[i].x < MAP_SIZE-16 && nav[i].y < MAP_SIZE-16)) {
+			if (map[nav[i].x][nav[i].y] == UNVISITED) {
 				Cell *new_valid = (Cell*)malloc(sizeof(Cell));
 				new_valid->x = nav[i].x/CELL_SIZE;
 				new_valid->y = nav[i].y/CELL_SIZE;
@@ -64,7 +66,7 @@ Cell* sort_cell(int **map, int x, int y) {
 	}
 
 	if (valid_nav->next) {
-		int value = randomizeInt(valid_nav->array);
+		int value = randomizeInt(valid_nav->array)+1;
 		Cell *temp;
 		for (value; value > 0; value--) {
 			temp = valid_nav;
@@ -80,10 +82,10 @@ Cell* sort_cell(int **map, int x, int y) {
 void generate(int **map) {
 	int count = 0;
 	Cell *cell;
-	while (!isEmpty() && count < 4) {
+	while (!isEmpty() && count < 8) {
 		cell = sort_cell(map, top().x, top().y);
 		if (cell) {
-			carve_cell(map, *cell);
+			//carve_cell(map, *cell);
 			push(cell);
 		} else {
 			pop();
@@ -105,13 +107,15 @@ void create_map(int **map) {
 		}
 		Cell *first_cell = (Cell*)malloc(sizeof(Cell));
 
-		first_cell->x = randomizeInt(MAP_SIZE/CELL_SIZE);
-		first_cell->y = randomizeInt(MAP_SIZE/CELL_SIZE);
+		int max = MAP_SIZE/CELL_SIZE;
+		
+		first_cell->x = rand()%(max-1);
+		first_cell->y = rand()%(max-1);
 
 		push(first_cell);
 		
 		carve_cell(map, *first_cell);
-		generate(map);
+		//generate(map);
 		
 	}
 }
